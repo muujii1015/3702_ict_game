5f5edc99e93ecde06cc4f4272301de00039844f6
yasheecool <140674298+yasheecool@users.noreply.github.com>
2023-10-06 22:11:58 +1000

    Added Shooting sound effect, bullet instantiation

--- Game_alpha/Assets/Scripts/Gun.cs
+++ Game_alpha/Assets/Scripts/Gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
@@ -10,11 +11,18 @@ public class Gun : MonoBehaviour
     public GameObject impact;
     public GameObject MuzzleFlash;
 
+    public GameObject bulletPrefab;
+    public Transform muzzle;
+
+    private AudioSource audioSource;
+
     private float timeToFire = 0f;
 
     void Start()
     {
         MuzzleFlash.SetActive(false);
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -23,8 +31,9 @@ public class Gun : MonoBehaviour
         {
             timeToFire = Time.time + 1f / FireRate;
             Shoot();
+            audioSource.Play();
 
-            PlayMuzzleFlash();          
+            PlayMuzzleFlash();
 
         }
     }
@@ -44,10 +53,18 @@ public class Gun : MonoBehaviour
                 target.TakeDamage(damage);
             }
 
+            if (bulletPrefab != null)
+            {
+                GameObject bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.identity);
 
+                // Start bullet movement towards hit point
+                StartCoroutine(MoveBullet(bullet, hitinfo.point));
+                Destroy(bullet, .75f);
+            }
 
             GameObject impactEffect = Instantiate(impact, hitinfo.point, Quaternion.LookRotation(hitinfo.normal));
             Destroy(impactEffect, .15f);
+            
         }
     }
 
@@ -66,10 +83,25 @@ public class Gun : MonoBehaviour
         MuzzleFlash.SetActive(false);
     }
 
-    
-     void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawRay(playerCam.transform.position, playerCam.transform.forward * range);
+    }
+
+    IEnumerator MoveBullet(GameObject bullet, Vector3 targetPosition)
+    {
+        float startTime = Time.time;
+        float duration = .04f; // Adjust this to control the bullet travel speed
+
+        while (Time.time - startTime < duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            bullet.transform.position = Vector3.Lerp(muzzle.position, targetPosition, t);
+            yield return null;
+        }
+
+        // Ensure the bullet reaches the target position
+        bullet.transform.position = targetPosition;
     }
 }
